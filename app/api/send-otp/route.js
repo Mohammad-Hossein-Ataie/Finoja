4e1b034dd54fa3d2e4b2d93edf2f54e67aed725c
Mohammad Hossein Ataie <mh.ataie@ut.ac.ir>
2025-07-14 21:20:03 +0330

    Fix send otp message.

--- app/api/send-otp/route.js
+++ app/api/send-otp/route.js
@@ -1,11 +1,12 @@
-import { sendOtpSMS } from '../../../lib/sendOtp';
-import dbConnect from '../../../lib/dbConnect';
-import OTP from '../../../models/OTP';
+import { sendOtpSMS } from "../../../lib/sendOtp";
+import dbConnect from "../../../lib/dbConnect";
+import OTP from "../../../models/OTP";
 
 export async function POST(req) {
   await dbConnect();
   const { mobile, type } = await req.json();
-  if (!/^09\d{9}$/.test(mobile)) return Response.json({ error: 'شماره معتبر نیست' }, { status: 400 });
+  if (!/^09\d{9}$/.test(mobile))
+    return Response.json({ error: "شماره معتبر نیست" }, { status: 400 });
 
   const code = Math.floor(100000 + Math.random() * 900000); // 6 رقمی
   await OTP.findOneAndUpdate(
@@ -13,10 +14,11 @@ export async function POST(req) {
     { code, createdAt: new Date(), type },
     { upsert: true, new: true }
   );
-  const text = type === 'register'
-    ? `کد ثبت‌نام: ${code}`
-    : `کد بازیابی رمز: ${code}`;
+  const text =
+    (type === "register" ? `کد ثبت‌نام: ${code}` : `کد بازیابی رمز: ${code}`) +
+    "\nفینوجا";
   const result = await sendOtpSMS(mobile, code, text);
-  if (!result.success) return Response.json({ error: result.message }, { status: 500 });
+  if (!result.success)
+    return Response.json({ error: result.message }, { status: 500 });
   return Response.json({ success: true });
 }
